1227f3b8dc997685c0ba3f1778c06584d692467e
Modory <dashkarni@gmail.com>
2026-04-09 22:38:06 +0300

    init: solution in function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const num = Math.abs(n);
+  const arr = String(num).split('');
+  let i = arr.length - 1;
+  while (arr[i] === '0') {
+    arr.pop();
+    i -= 1;
+  }
+  return Number(arr.reverse().join(''));
 };
